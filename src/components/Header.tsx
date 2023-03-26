2a981f02a1b1b58f92c730beee45541f465d5a3d
StefanWiencko <stefanwiencko2@gmail.com>
2023-03-26 20:21:36 +0200

    feat: init commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import logo from '@/assets/gnStudio-logo.jpeg'
+
+export const Header = () => {
+  return (
+    <header className='flex h-28 px-12 py-4'>
+        <img  src={logo} alt='gnStudio logo'></img>
+    </header>
+  );
+};
