f1b63c3297623e5d9c4437810ff7733e8c8c3c64
StefanWiencko <stefanwiencko2@gmail.com>
2023-03-26 20:22:26 +0200

    feat: init commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/components/Header";
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+function App() {
+  return (
+    <div className="h-screen bg-BRAND-bgPrimary">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        BRAND: {
+          bgPrimary: "#0e1217",
+          bgSecondary: "#1c1f26",
+          border: "#a8b3cf33",
+          primaryPurple: "#ce3df3",
+          secondaryPurple: "#ac1de4",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
